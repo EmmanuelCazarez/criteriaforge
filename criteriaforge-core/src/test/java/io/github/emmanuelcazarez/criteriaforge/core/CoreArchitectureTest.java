0f672b7e23ec697e731b00c07dccea7f03c8da1e
Emmanuel Cazarez Perea <emmanuelcazarez98@gmail.com>
2026-07-20 16:48:27 -0700

    test(core): keep consumer API cohesive

--- criteriaforge-core/src/test/java/io/github/emmanuelcazarez/criteriaforge/core/CoreArchitectureTest.java
+++ criteriaforge-core/src/test/java/io/github/emmanuelcazarez/criteriaforge/core/CoreArchitectureTest.java
@@ -1,16 +1,28 @@
 package io.github.emmanuelcazarez.criteriaforge.core;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import org.junit.jupiter.api.Test;
 
 class CoreArchitectureTest {
 
+    private static final String CORE_PACKAGE =
+        "io.github.emmanuelcazarez.criteriaforge.core";
+
+    @Test
+    void consumerApiRemainsInSingleCorePackage() {
+        assertThat(QuerySpec.class.getPackageName()).isEqualTo(CORE_PACKAGE);
+        assertThat(FilterExpression.class.getPackageName()).isEqualTo(CORE_PACKAGE);
+        assertThat(QueryPolicy.class.getPackageName()).isEqualTo(CORE_PACKAGE);
+        assertThat(CriteriaForgeException.class.getPackageName()).isEqualTo(CORE_PACKAGE);
+    }
+
     @Test
     void coreRemainsTransportAndPersistenceIndependent() {
         var classes = new ClassFileImporter()
-            .importPackages("io.github.emmanuelcazarez.criteriaforge.core");
+            .importPackages(CORE_PACKAGE);
 
         noClasses().should().dependOnClassesThat().resideInAnyPackage(
             "org.springframework..",
